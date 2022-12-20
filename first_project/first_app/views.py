583a8707cf30a8ebfbc4422c428dd563a142de02
Jie Hu <jiehu@Jies-MacBook-Pro.local>
2022-12-20 14:27:10 -0500

    test the page with path converter

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -7,5 +7,11 @@ def home(request):
     return HttpResponse("Welcome to the home page!")
 def educative(request):
     return HttpResponse("Welcome to the educative page!")
-def show_age(request, age):
-    return HttpResponse("I am %s years old." % age)
+# def show_age(request, age):
+#     return HttpResponse("I am %s years old." % age)
+def even_or_odd(request, num):
+    if(num % 2 == 0):
+        output = "%s is an even number." % num
+    else:
+        output = "%s is an odd number." % num
+    return HttpResponse(output)
--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('home/', views.home, name="home"),
     path('educative/', views.educative, name='educative'),
-    path('<age>/', views.show_age, name='show_age'),
+    # path('<age>/', views.show_age, name='show_age'),
+    path('<int:num>/', views.even_or_odd, name='even_or_odd'),
 ]
