52b8bb029869d30ceae9319292fac513628b4a1b
Jie Hu <jiehu@Jies-MacBook-Pro.local>
2022-12-20 14:05:50 -0500

    add home and educative page

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello World!")
+def home(request):
+    return HttpResponse("Welcome to the home page!")
+def educative(request):
+    return HttpResponse("Welcome to the educative page!")
--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -20,4 +20,6 @@ from first_app import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('home/', views.home, name="home"),
+    path('educative/', views.educative, name='educative'),
 ]
