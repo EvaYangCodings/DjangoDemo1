bba19a2a6a75f9e8f2d507a8033e0774bd1edb94
Jie Hu <jiehu@Jies-MacBook-Pro.local>
2022-12-21 13:23:15 -0500

    add return keyword

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -5,7 +5,7 @@ def index(request):
     # favorite_book = {'Raymond':"Book1", 'Emma': 'book2'}
     # return render(request, 'first_app/index.html', context=favorite_book)
     tv_shows_list={"tv_shows":{'Game of Thrones':'9.3','Blacklist': '8','Suits': '8.5','The Witcher': '8.5'}}
-    render(request, 'first_app/index.html', context=tv_shows_list)
+    return render(request, 'first_app/index.html', context=tv_shows_list)
 def home(request):
     return HttpResponse("Welcome to the home page!")
 def educative(request):
