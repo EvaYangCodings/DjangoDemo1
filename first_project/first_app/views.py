169a07a1237839005527a964343b1da7c0076a31
Jie Hu <jiehu@Jies-MacBook-Pro.local>
2022-12-21 12:57:56 -0500

    the usage of variables

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World!")
+    favorite_book = {'Raymond':"Book1", 'Emma': 'book2'}
+    return render(request, 'first_app/index.html', context=favorite_book)
 def home(request):
     return HttpResponse("Welcome to the home page!")
 def educative(request):
